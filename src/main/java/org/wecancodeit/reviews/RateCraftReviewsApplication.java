4de99b6fde5c2b9ed1133be548fe24a579342e30
Pete Schaaf <peteschaaf.19@gmail.com>
2020-10-09 14:54:41 -0400

    beginning of starting template for review page data

--- src/main/java/org/wecancodeit/reviews/RateCraftReviewsApplication.java
+++ src/main/java/org/wecancodeit/reviews/RateCraftReviewsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReviewsApplication {
+public class RateCraftReviewsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ReviewsApplication.class, args);
+        SpringApplication.run(RateCraftReviewsApplication.class, args);
     }
 
 }
