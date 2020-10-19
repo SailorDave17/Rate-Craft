003bee1dbf63439560885d547aa7a0ef7b843f00
Dave <HSC.Coach@gmail.com>
2020-10-19 14:30:34 -0400

    10/19 230

--- src/main/java/org/wecancodeit/reviews/controllers/CategoryController.java
+++ src/main/java/org/wecancodeit/reviews/controllers/CategoryController.java
@@ -3,7 +3,9 @@ package org.wecancodeit.reviews.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.wecancodeit.reviews.storage.CategoryStorage;
 import org.wecancodeit.reviews.storage.ReviewStorage;
 
@@ -26,5 +28,13 @@ public class CategoryController {
     public String displayCategoryPage(Model model, @PathVariable long id) {
         model.addAttribute("category", categoryStorage.retrieveCategoryById(id));
         return "category";
+    }
+
+    @PostMapping("/category")
+    public String createCategoryAndReview(@RequestParam String bcategory, @RequestParam String bmakename, @RequestParam String bmodelname, @RequestParam String description){
+//        reviewCategory categoryToAdd = new ReviewCatagory(categoryName);
+//        catagoryStorage.addCategory(categoryToAdd);
+
+
     }
 }
